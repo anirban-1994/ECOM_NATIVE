e4efd9c6968c72e3c54c9aaab5ea85c124b83815
Anirban Dutta <63803805+anirban-1994@users.noreply.github.com>
2022-09-10 18:18:39 +0530

    main.tsx added

--- src/Screens/Main.tsx
+++ src/Screens/Main.tsx
@@ -6,15 +6,18 @@ import {Renewal} from './Renewal/Renewal';
 import {Contact} from './Contact/Contact';
 
 const Tab = createMaterialTopTabNavigator();
+
 export const Main = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="SiteLog" component={SiteLog} />
-        <Tab.Screen name="AMC" component={AMC} />
-        <Tab.Screen name="Renewal" component={Renewal} />
-        <Tab.Screen name="Contact" component={Contact} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="SiteLog" component={SiteLog} />
+          <Tab.Screen name="AMC" component={AMC} />
+          <Tab.Screen name="Renewal" component={Renewal} />
+          <Tab.Screen name="Contact" component={Contact} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
