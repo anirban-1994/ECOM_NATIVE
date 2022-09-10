d124f7668b7d2f88ecbf2f5f9374fa484e306f08
Anirban Dutta <63803805+anirban-1994@users.noreply.github.com>
2022-09-10 19:06:31 +0530

    Revert "Drawer added"
    This reverts commit e8d820ab024dac99d30052ceb8d68db5690c9b60.

--- src/Screens/Blog/Blog.tsx
+++ src/Screens/Blog/Blog.tsx
@@ -1,5 +1,3 @@
-import {Text} from 'react-native';
-
-export const Blog = () => {
-  return <Text>Message from blog</Text>;
-};
+export const Blog = ()=>{
+    
+}
--- src/Screens/Main.tsx
+++ src/Screens/Main.tsx
@@ -4,22 +4,13 @@ import {AMC} from './AMC/AMC';
 import {NavigationContainer} from '@react-navigation/native';
 import {Renewal} from './Renewal/Renewal';
 import {Contact} from './Contact/Contact';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import {About} from './About/About';
-import {Blog} from './Blog/Blog';
 
-const Drawer = createDrawerNavigator();
 const Tab = createMaterialTopTabNavigator();
 
 export const Main = () => {
   return (
     <>
       <NavigationContainer>
-        <Drawer.Navigator>
-          <Drawer.Screen name="About" component={About} />
-          <Drawer.Screen name="Contact" component={Contact} />
-          <Drawer.Screen name="Blog" component={Blog} />
-        </Drawer.Navigator>
         <Tab.Navigator>
           <Tab.Screen name="SiteLog" component={SiteLog} />
           <Tab.Screen name="AMC" component={AMC} />
